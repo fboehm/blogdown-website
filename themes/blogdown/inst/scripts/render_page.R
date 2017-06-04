0b685cfed947b85e324a59c2fa1fe58bb4c9e3ae
Frederick Boehm <frederick.boehm@gmail.com>
2017-06-04 13:04:48 -0500

    deleted themes dir

--- themes/blogdown/inst/scripts/render_page.R
+++
@@ -1,7 +0,0 @@
-local({
-  options(htmltools.dir.version = FALSE)
-  args = commandArgs(TRUE)
-  rmarkdown::render(
-    args[1], 'blogdown::html_page', envir = globalenv(), quiet = TRUE, encoding = 'UTF-8'
-  )
-})
